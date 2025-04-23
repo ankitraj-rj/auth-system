a9392c9dc5919fd402947de82978e33c0a580b1e
Ankit Raj <ankitrajcollab@gmail.com>
2025-04-24 02:39:54 +0530

    add: nodemailer with mailtrap service

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/User.model.js";
 import crypto from "crypto";
+import nodemailer from "nodemailer";
 
 const registerUser = async (req, res) => {
   // Get Data From User
@@ -48,9 +49,30 @@ const registerUser = async (req, res) => {
     user.verificationToken = token;
     await user.save();
 
+    //Send Mail
+
+    const transporter = nodemailer.createTransport({
+      host: process.env.MAILTRAP_HOST,
+      port: process.env.MAILTRAP_PORT,
+      secure: false, // true for port 465, false for other ports
+      auth: {
+        user: process.env.MAILTRAP_USERNAME,
+        pass: process.env.MAILTRAP_PASSWORD,
+      },
+    });
+
+    const mailOptions = {
+      from: process.env.MAILTRAP_SENDEREMAIL, // sender address
+      to: User.email, // list of receivers
+      subject: "Verigy your mail id", // Subject line
+      text: `please click on url link: ${process.env.BASE_URL}/api/v1/users/verify/${token}`, // plain text body
+      html: "<b>Hello world?</b>", // html body
+    };
+  } catch (error) {
     
-  } catch (error) {}
+  }
 };
+
 
 const loginUser = async (req, res) => {
   res.send("loggedin Successfully");
